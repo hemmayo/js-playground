d4ea1351ec5fbdfd184fe0c36a0fab0dfcf84137
Emmanuel Popoola <olaayo10@gmail.com>
2019-03-16 20:40:52 +0100

    updates

--- oop/hangman.js
+++ oop/hangman.js
@@ -2,12 +2,28 @@
 // Setup attributes for the word itself and the number of guesses allowed
 // Create two instances of it and print both to the console
 
+// Setup the word instance property as an array of lower case letters
+// Setup another instance property to store guessed letters
+// Create a method that gives you the word puzzle back
+
+// No guesses -> ***
+// Guessed "c", "b", "t" -> c*t
+
 const Hangman = function (word, remainingGuesses) {
-    this.word = word
+    word.forEach((letter) => letter = letter.toLowerCase())
+    this.word = word.split()
+    this.guesses = []
     this.remainingGuesses = remainingGuesses
 }
 
-const game1 = new Hangman('cat', 3)
+Hangman.prototype.getPuzzle = function () {
+    const word = this.word
+    word.forEach((letter) => {
+        letter = '*'
+    })
+}
+
+const game1 = new Hangman('Caterpiller', 3)
 const game2 = new Hangman('cat', 3)
 
-console.log(game1, game2)
+console.log(game1.getPuzzle())
