e019d684df002f853238d1afe97a564d16a84297
Emmanuel Popoola <olaayo10@gmail.com>
2019-03-16 20:30:44 +0100

    updates

--- oop/person.js
+++ oop/person.js
@@ -1,8 +1,26 @@
-const Person = function (firstName, lastName, age) {
+// Prototypal inheritance
+
+const Person = function (firstName, lastName, age, likes = []) {
     this.firstName = firstName
     this.lastName = lastName
     this.age = age
+    this.likes = likes
 }
 
-const me = new Person('Emmanuel', 'Popoola', 19)
-console.log(me)
+Person.prototype.getBio = function () {
+    let bio = `${this.firstName} is ${this.age}.`
+    this.likes.forEach((like) => {
+        bio += ` ${this.firstName} likes ${like}.`
+    })
+    return bio
+}
+
+Person.prototype.setName = function (fullName) {
+    const names = fullName.split(' ')
+    this.firstName = names[0]
+    this.lastName = names[1] ? names[1] : ''
+}
+
+const me = new Person('Emmanuel', 'Popoola', 19, ['Teaching', 'Singing'])
+me.setName('Emma Popoola')
+console.log(me, me.getBio())
