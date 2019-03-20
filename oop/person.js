1f4053fadd2eec2a603471e30239cf2db9f9035e
Emmanuel Popoola <olaayo10@gmail.com>
2019-03-20 17:51:07 +0100

    Subclasses

--- oop/person.js
+++ oop/person.js
@@ -21,6 +21,35 @@ class Person {
     }
 }
 
-const me = new Person('Emmanuel', 'Popoola', 19, ['Teaching', 'Singing'])
-me.setName('Emma Popoola')
-console.log(me, me.getBio())
+class Employee extends Person {
+    constructor(firstName, lastName, age, position, likes) {
+        super(firstName, lastName, age, likes)
+        this.position = position
+    }
+    getBio () {
+        let bio = `${this.firstName} is a ${this.position}.`
+        return bio
+    }
+    getYearsLeft () {
+        return 65 - this.age
+    }
+}
+
+class Student extends Person {
+    constructor (firstName, lastName, age, likes, grade = 0) {
+        super(firstName, lastName, age, likes)
+        this.grade = grade
+    }
+    getBio () {
+        return `${this.firstName} is ${this.grade >= 70 ? 'passing' : 'failing'} the cutoff mark.`
+    }
+    updateGrade (grade) {
+        this.grade += grade
+        this.grade = this.grade > 100 ? 100 : this.grade < 0 ? 0 : this.grade
+    }
+}
+
+const me = new Student('Emmanuel', 'Popoola', 19, ['eating'], 10)
+console.log(me.getBio())
+me.updateGrade(72)
+console.log(me.getBio())
