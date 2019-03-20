a7bf263720153086a33120eb42508b94e5e7c370
Emmanuel Popoola <olaayo10@gmail.com>
2019-03-20 19:43:38 +0100

    using apis

--- hangman/app.js
+++ hangman/app.js
@@ -1,14 +1,27 @@
-const game = new Hangman('Car parts', 5)
+let game
 const puzzleElement = document.querySelector('#puzzle')
 const status = document.querySelector('#status')
 
 window.addEventListener('keypress', function (e) {
-    const guess = e.key
     game.guess(e.key)
-    
     puzzleElement.textContent = game.puzzle
     status.textContent = game.statusMessage
 })
 
-puzzleElement.textContent = game.puzzle
-status.textContent = game.statusMessage
+const request = new XMLHttpRequest()
+request.addEventListener('readystatechange', function (e) {
+    if (e.target.readyState === 4 && e.target.status === 200) {
+        let data = []
+        try {
+            data = JSON.parse(e.target.responseText)
+            game = new Hangman(data.puzzle, 5)
+            puzzleElement.textContent = game.puzzle
+            status.textContent = game.statusMessage
+        } catch (e) {
+            status.textContent = 'Unavailable'
+        }
+    }
+})
+request.open('GET', 'http://puzzle.mead.io/puzzle')
+request.send()
+
