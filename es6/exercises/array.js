784d135a379590e770bee0152b26ed6ec1722488
Emmanuel Popoola <olaayo10@gmail.com>
2019-03-21 19:12:27 +0100

    string literals

---
+++ es6/exercises/array.js
@@ -0,0 +1,11 @@
+// Reduce
+const array1 = [2, 5, 6, 7, 8]
+const b = array1.reduce((accumuator, num) => accumuator + num, 0)
+console.log(b)
+
+// Map
+var array2 = [1, 4, 9, 16];
+// pass a function to map
+const map1 = array2.map((x) => x + 2);
+
+console.log(map1);
