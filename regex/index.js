bd19d26eea616d9a78967c283ca2b683cb48d4d3
Emmanuel Popoola <olaayo10@gmail.com>
2019-03-29 21:31:59 +0100

    regex update

--- regex/index.js
+++ regex/index.js
@@ -1,3 +1,3 @@
-let string = 'emma'
-let regex = new RegExp('a', 'g');
+let string = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, earum expedita asperiores voluptatibus distinctio harum, aspernatur vero repellat impedit, reiciendis sint ab quos itaque soluta tempore dolore. Expedita, consequuntur quos.'
+let regex = new RegExp('star', 'g');
 console.log(regex.test(string))
